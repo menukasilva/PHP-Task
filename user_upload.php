f62a0c92d8b42f21543c732da572bed611936971
menukasilva <49965920+menukasilva@users.noreply.github.com>
2023-03-16 14:41:53 -0300

    Connect to database
    establishes a connection to a MySQL database using the PDO

--- user_upload.php
+++ user_upload.php
@@ -17,8 +17,19 @@ if (!file_exists($filename)){
     exit("Error:File does not exist\n");
 }
 
+//Connect to database
+$servername = $options['servername'];
+$username = $options['username'];
+$password = $options['password'];
+$database = $options['database'];
 
-
-
+try{
+    $conn = new PDO("mysql:host=$servername; dbname=$database",  $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+    echo "Connected to database";
+}catch(PDOException $e){
+    echo "Error connecting to database.";
+    exit();
+}
 
 ?>
