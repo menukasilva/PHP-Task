9dd8d2712d6e302e3ef6e019b84348aa7dca01ee
menukasilva <49965920+menukasilva@users.noreply.github.com>
2023-03-16 15:20:54 -0300

    Create user table and insert into db
    Iterate through rows, capitalize names and execute insert statement

--- user_upload.php
+++ user_upload.php
@@ -32,4 +32,46 @@ try{
     exit();
 }
 
+//Create users table if not already exists
+try{
+    $conn->execute("CREATE TABLE IF NOT EXISTS 'users'(
+        'id' INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+        'name' VARCHAR(255) NOT NULL,
+        'surname' VARCHAR(255) NOT NULL,
+        'email' VARCHAR(255) NOT NULL,)
+        ENGINE = InnoDB;");
+}catch(PDOException $e){
+    echo "Error Could not create users table." .$e->getMessage() . "\n";
+    exit(1);
+}
+
+//Prepare insert statement
+$insert = $conn->prepare("INSERT INTO users,('name', 'password', 'email') VALUES (?, ?, ?)");
+
+//Iterate through rows and insert into database table
+foreach ($rows as $row) {
+    //Check if email is valid
+    if(!filter_var($row[2], FILTER_VALIDATE_EMAIL)){
+        echo "Email is not valid. Skipping the row.\n";
+        continue;
+    }
+
+    //Capitalize name and surname
+    $name = ucfirst(strtolower($row[0]));
+    $surname = ucfirst(strtolower($row[1]));
+    $email = strtolower($row[2]);
+
+    //Execute insert statement
+    try{
+        $insert->execute([$name,$surname,$email]);
+    }catch(PDOException $e){
+        echo "Error inserting row." .$e->getMessage() . "\n";
+    }
+}
+
+//Close database connection
+$conn = null;
+
+echo "Done.\n";
+
 ?>
